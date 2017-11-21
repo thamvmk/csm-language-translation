4736fd3cf65d5cc1888587802381a225ad4ec9f6
Vincent Tham <thamvmk@yahoo.com>
2017-11-21 20:55:02 +0800

    updated split_long_text script

--- mysql-query.py
+++ mysql-query.py
@@ -4,23 +4,47 @@
 import datetime
 import MySQLdb
 import sys
+import re
+from translate import trans
 
 connection = MySQLdb.connect (host = "dev.kewmann.com", user = "vincent_tham", passwd = "P@$$w0rd", db = "csm")
-
 cursor = connection.cursor ()
-
 
 query = ("SELECT url, content, lastupdated FROM csm.online_news WHERE LastUpdated BETWEEN %s AND %s")
 
-lastupdate_start = datetime.date(2016, 12, 6)
-lastupdate_end = datetime.date(2016, 12, 7)
+lastupdate_start = datetime.date(2016, 12, 9)
+lastupdate_end = datetime.date(2016, 12, 10)
 
 cursor.execute(query, (lastupdate_start, lastupdate_end))
 
 for ( url, content, lastupdated) in cursor:
+    translated_text_en    = ""
+    translated_text_zh_cn = ""
+    a = ""
     print ("URL        : %s" % url)
     print ("content    : %s" % content)
     print ("lastupdated: %s\n" % lastupdated)
-
+    #content = content.replace("\"","")
+    #content = content.replace("\'","")
+    if ('\"' in content):
+        print ("Yes, content has \"\n")
+        a = re.sub('[^A-Za-z0-9\.\"\?\s\w]+', ' ', content)
+        print ("a = %s" % a)
+        input("Press Enter to continue...")
+    elif ('\'' in content):
+        print ("Yes, content has \'\n")
+        a = re.sub('[^A-Za-z0-9\.\"\?\s\w]+', ' ', content)
+        #a = re.sub(r'[\'|"|“|\|\[|]}“]',r'',content)
+        #a = a.replace('`', '')
+        print ("a = %s" % a)
+        input("Press Enter to continue...")
+    else:
+        a = content
+    #print ("content    : %s" % a)
+    translated_text_en    = trans(a, "en") 
+    print ("content_en : %s" % translated_text_en)
+    translated_text_zh_cn = trans(a, "zh-cn") 
+    print ("content_ch : %s" % translated_text_zh_cn)
+    input("Press Enter to continue...")
 cursor.close()
 connection.close()
--- split_long_text.py
+++ split_long_text.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
 
-#print (translator.translate('I want to go to toilet.', dest='zh-cn'))
 
-def trans( content,  lang ):
-    from googletrans import Translator
-    translator = Translator()
-    out_lang = ""
+def split_long_text( content ):
     "This prints a passed info into this function"
-    print ("Content  : ", content)
-    print ("Lang     : ", lang)
-    out_lang = translator.translate(content, lang) 
-    #print ("Out_lang : ", out_lang.text)
+    import re
+    print ("content = %s" % content)
+    m = re.split(r'(?<=[^A-Z].[.?]) +(?=[A-Z])', content)
+    sentences = []
+    t_ele = 0
+    max_words = 20
+    for i in m:
+        print (i)
+        t_ele = len(sentences)
+        if (t_ele == 0):
+            sentences.append(i)
+        elif (t_ele > 0):
+            last_element = sentences[t_ele - 1]
+            #print ("last sentence [%s]" % last_element)
+            word_count = len(last_element.split())
+            if (word_count <= max_words):
+                sentences[t_ele - 1] = sentences[t_ele - 1] + " " + i 
+            else:
+                sentences.append(i)
+            #print ("last array sentence [%s]" % sentences[t_ele - 1])
+            word_count = len(sentences[t_ele - 1].split())
+            #print ("word count [%d]" % word_count)
+            #input("Press Enter to continue...")
+        #t_ele = len(sentences)
+        #print ("total element: %d" % t_ele)
+            
+    for num, line in enumerate(sentences):
+        print ("item [%d] [%s]" % (num, line))
+
     #input("Press Enter to continue...")
-    return out_lang.text;
+    return;
+
+if __name__ == '__main__':
+    print("This only executes when %s is executed rather than imported" % __file__)
+    split_long_text("""Mr. Smith bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't.""")
+
